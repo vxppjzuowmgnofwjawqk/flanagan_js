b14f2ede88e6f69d5825448b14c0b722d214a2b2
сергей супрунчук <suprunchuk@isands.ru>
2022-05-06 15:38:08 +0700

    js

--- practice.js
+++ practice.js
@@ -95,3 +95,179 @@
 //
 // const date = new Date() => Объект
 // const timestamp = Date.now() => Числовое представление
+//
+// const string1 = 'HELLO, '
+// const string2 = 'WORLD!'
+//
+// console.log(string1 + string2) => "HELLO, WORLD!"
+//
+// const string = 'STRING'
+//
+// console.log(string.toLowerCase()) => "string"
+// console.log(string.toUpperCase()) => "STRING"
+// console.log(string.includes('TRI')) => true
+// console.log(string.startsWith('STR')) => true
+// console.log(string.endsWith('ING')) => true
+// console.log(string.indexOf('S')) => 0
+// console.log(string.indexOf('S', 1)) => -1
+// console.log(string.lastIndexOf('N')) => 4
+// console.log(string.slice(1, 3)) => "TR"
+// console.log(string.replace('STRING', 'NUMBER')) => "NUMBER"
+// console.log(string.padStart(10, '*')) => "****STRING"
+// console.log(string.padEnd(10, '*')) => "STRING****"
+// console.log(string.length) => 6
+// console.log(string.repeat(3)) => "STRINGSTRINGSTRING"
+// console.log(string === string.toLowerCase()) => false
+//
+// const name = 'СЕРГЕЙ'
+// const age = 19
+//
+// const string1 = `ДОБРЫЙ ДЕНЬ, МЕНЯ ЗОВУТ ${name} И МНЕ ${age} ЛЕТ!`
+// const string2 = f`ДОБРЫЙ ДЕНЬ, МЕНЯ ЗОВУТ ${name} И МНЕ ${age} ЛЕТ!`
+//
+// function f(strings, name, age) {
+// 	return strings[0] + name + strings[1] + age + strings[2]
+// }
+//
+// console.log(string1 === string2)
+//
+// const text = 'testing: 3.. 2.. 1.. !!'
+// const pattern = /\d+/g
+//
+// console.log(text.match(pattern))
+// console.log(pattern.test(text))
+// console.log(text.replace(pattern, '#'))
+//
+// const _true = true
+// const _false = false
+//
+// console.log(_false.toString())
+//
+// if (0 && -0) console.log(true); else console.log(false)
+//
+// if (null && undefined) console.log(true); else console.log(false)
+//
+// if (NaN && '') console.log(true); else console.log(false)
+//
+// console.log(10 <= 0 || 10 >= 0)
+//
+// const _null = null
+// const _undefined = undefined
+//
+// console.log(_null == _undefined)
+//
+// console.log(!!_null)
+// console.log(!!_undefined)
+//
+// _undefined.toString() => TypeError
+//
+// const object = {}
+//
+// const symbol = Symbol('id')
+// const symbol1 = Symbol.for('id')
+// const symbol2 = Symbol.for('id')
+//
+// object[symbol] = Math.random().toString().replace('0.', '#')
+//
+// console.log(object[symbol])
+//
+// object[symbol1] = Math.random().toString().replace('0.', '#')
+//
+// console.log(object[symbol1])
+// console.log(object[symbol2])
+// console.log(object[Symbol.for(Symbol.keyFor(symbol1))])
+//
+// console.log(symbol1.toString())
+//
+// console.log(global === globalThis)
+// console.log(window)
+//
+// const o1 = {email: '@mail.ru'}
+// const o2 = {email: '@mail.ru'}
+// const o3 = o1
+//
+// console.log(o1 === o2)
+// console.log(o1 === o3)
+//
+// console.log(0 === 0)
+// console.log(true === true)
+// console.log('STRING' === 'STRING')
+//
+// o3.email = o3.email.replace('@', '#')
+// console.log(o1.email)
+//
+// const s1 = 'STRING'
+// s1[0] = '#'
+// console.log(s1)
+// console.log(10..toString())
+// console.log(true.toString())
+// console.log(undefined + '')
+// console.log(null + '')
+// console.log([] + 'STRING')
+// console.log([10].toString())
+// console.log(['STRING', 10] + '')
+//
+// console.log('10' / 10)
+// console.log('10 S' / 10)
+// console.log(true - false)
+// console.log(undefined - 10)
+// console.log(null - 10)
+// console.log([] - 10)
+// console.log([100] - 10)
+// console.log([100, 400] - 10)
+//
+// console.log(!!'')
+// console.log(!!null)
+// console.log(!!undefined)
+// console.log(!!0)
+// console.log(!!-0)
+// console.log(!!NaN)
+//
+// console.log('10' == 10)
+// console.log('10' === 10)
+//
+// console.log(String(10))
+// console.log(Number('10'))
+// console.log(Number('10n'))
+// console.log(Boolean(10))
+//
+// console.log(parseInt(' 10.1n'))
+// console.log(parseInt(' 0xff'))
+// console.log(parseInt('ff', 16))
+//
+// console.log(parseFloat(' 10.1n'))
+//
+// console.log(10.125.toFixed(1))
+// console.log(10.125.toPrecision(2))
+// console.log(10.125.toExponential(3))
+//
+// const o = {
+//
+// 	valueOf() {
+//
+// 		return 1
+//
+// 	},
+//
+// 	toString() {
+//
+// 		return 'STRING'
+//
+// 	}
+//
+// }
+// console.log(String(o))
+// console.log(Number(o))
+// console.log(Boolean(o))
+//
+// let a
+// let b = 40, c = b + 20
+//
+// const PI = 3.14
+//
+// const POSITIVE_INFINITY = Infinity
+//
+// console.log(a)
+// console.log(c)
+//
+// PI = null => TypeError
